f78f2bdfc0eba8f7a3964f95f5f1eaa05451da4e
Towhid Hasan <towhid.hasan11@gmail.com>
2020-05-09 20:21:33 +0600

    Add a line

---
+++ app/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Home.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container} data-tid="container">
+      <h2>Home</h2>
+      <h2>Test</h2>
+    </div>
+  );
+}
